0e76dc9336e91103965ac897454c2fe0365a3a9e
natanaelfreire <natan.nascimento.freire@gmail.com>
2024-12-06 10:12:59 -0300

    renomea entity user

--- src/users/services/find-one-user.ts
+++ src/users/services/find-one-user.ts
@@ -1,4 +1,4 @@
-import { User } from "../entities/user.entity";
+import { User } from "../entities/user";
 import { UserRepository } from "../repositories/user-repository";
 
 export class FindOneUser {
--- src/users/services/update-user.ts
+++ src/users/services/update-user.ts
@@ -1,4 +1,4 @@
-import { User } from '../entities/user.entity'
+import { User } from '../entities/user'
 import { UserRepository } from '../repositories/user-repository'
 import { UpdateUserDto } from '../dto/update-user.dto'
 
